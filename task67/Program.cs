3539551764a35996ea0b48bd13f8f5fb33ae5d6f
Semen <semgenomsk@yandex.ru>
2022-10-03 21:57:38 +0600

    Done task 67

---
+++ task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 67: Напишите программу, которая будет 
+// принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits(int number)
+{
+    if (number == 0) return 0;
+    else
+    {
+        return number % 10 + SumDigits(number / 10);
+    }
+}
+
+Console.Write(SumDigits(ReadNumber("Введите M: ")));
